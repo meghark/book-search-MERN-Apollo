b8ecf1097ec9a3616af2e4d1550b19579c6c4d16
meghark <98137756+meghark@users.noreply.github.com>
2022-07-07 19:55:47 -0700

    Return saved books details with user query

--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -8,6 +8,17 @@ const typeDefs = gql `
             username:  String
             email: String
             password: String
+            savedBooks: [Book]
+        }
+
+        type Book{
+            _id: ID
+            authors: [String]
+            description: String
+            bookId: String
+            image: String
+            link: String
+            title: String
         }
 
         type Query {
