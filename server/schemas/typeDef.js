561f0c2f4b5e805863501a0ac90723b864d052fb
meghark <98137756+meghark@users.noreply.github.com>
2022-07-07 17:12:30 -0700

    Server file update with Apollo server changes

---
+++ server/schemas/typeDef.js
@@ -0,0 +1,8 @@
+// gql is a tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql ``;
+
+// export the typeDefs
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -3,11 +3,41 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Import ApolloServer
+const {ApolloServer}  = require('apollo-server-express');
+// Import typeDefs and Resolvers
+const {typeDefs, resolvers} = require('./schemas');
+// Use the typeDefs and resolvers to create a new schema
+ const server = new ApolloServer({
+   typeDefs,
+   resolvers
+ });
+
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// create a new instance of the apollo server with the defined schema.
+const startApolloServer = async (typeDefs, resolvers) => {
+await server.start();
+
+  //Integrate server with express application
+server.applyMiddleware({ app });
+
+    db.once('open', () => {
+      app.listen(PORT, () => {
+            console.log(`🌍 Now listening on localhost:${PORT}`)
+
+        // Display path to test GQL Api
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+};
+
+// start the server
+startApolloServer(typeDefs, resolvers);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -16,6 +46,3 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
