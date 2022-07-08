ac9b4fb5fee776125171c1f1871f079c609c12a1
meghark <98137756+meghark@users.noreply.github.com>
2022-07-07 23:28:04 -0700

    Added apollo client setup for api connection

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,38 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// ApolloProvider provide data for all the components.
+// ApolloClient , intializes connection to the GraphQL API server.
+// InMemoryCache caches API response data to perform requests.
+// createHttpLink control Apollo client requests. Kind of like middleware for outbound network requests.
+import { ApolloProvider, ApolloClient, InMemoryCache,  createHttpLink} from '@apollo/client';
+
+// Connect to back-end server /graphql.
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Create connection to API end point via above link.
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// App will interact with above client instance.
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
